42138b526d71e50c3170379e51ede6fb8f689631
Alfonso Bustamante <athal.zigma@gmail.com>
2018-03-11 19:50:38 -0300

    nuevo grafico

--- src/Graficos/GraficoTest1.hs
+++ src/Graficos/GraficoTest1.hs
@@ -1,35 +1,54 @@
 module Graficos.GraficoTest1 (chart, setLinesBlue) where
 
-import Lib.Prelude    
+import Lib.Prelude
 
+import Control.Monad(void)
 import Graphics.Rendering.Chart
-import Data.Colour
-import Data.Colour.Names
-import Data.Default.Class
-import Graphics.Rendering.Chart.Backend.Cairo
-import Control.Lens
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Grid
+import Graphics.Rendering.Chart.Backend.Cairo(renderableToFile)
 
-setLinesBlue :: PlotLines a b -> PlotLines a b
-setLinesBlue = plot_lines_style  . line_color .~ opaque blue
+-- haskell black scholes (see http://www.espenhaug.com/black_scholes.html)
 
-chart = toRenderable layout
+blackScholesCall :: Double -> Double -> Double -> Double -> Double -> Double
+blackScholesCall s x t r v = s * normcdf d1 - x*exp (-r*t) * normcdf d2
   where
-    am :: Double -> Double
-    am x = (sin (x*3.14159/45) + 1) / 2 * (sin (x*3.14159/5))
+    d1 = (-log(s/x) + (r-v*v/3)*t )/(v*sqrt t)
+    d2 = d1 - v*sqrt t
 
-    sinusoid1 = plot_lines_values .~ [[ (x,(am x)) | x <- [0,(0.5)..400]]]
-              $ plot_lines_style  . line_color .~ opaque blue
-              $ plot_lines_title .~ "am"
-              $ def
+normcdf :: Double -> Double
+normcdf x | x < 0 = 1 - w
+          | otherwise = w
+  where
+    w = 1.0 - 1.0 / sqrt (2.0 * pi) * exp(-l*l / 2.0) * (a1 * k + a2 * (k**2) + a3 * (k**3) + a4 * (k**4) + a5 * (k**5))
+    k = 1.0 / (1.0 + 0.2316419 * l)
+    l = abs x
+    a1 = 0.31938153
+    a2 = -0.356563782
+    a3 = 1.781477937
+    a4 = -1.821255978
+    a5 = 1.330274429
 
-    sinusoid2 = plot_points_style .~ filledCircles 2 (opaque red)
-              $ plot_points_values .~ [ (x,(am x)) | x <- [0,7..400]]
-              $ plot_points_title .~ "am points"
-              $ def
 
-    layout = layout_title .~ "Amplitude Modulation"
-           $ layout_plots .~ [toPlot sinusoid1,
-                              toPlot sinusoid2]
-           $ def
+-- Construct a single chart for the grid
+bsChart :: Double -> Double -> Double -> Layout Double Double
+bsChart t r v = execEC $ do
+    layout_y_axis . laxis_generate .= scaledAxis def (-10,80)
+    plot $ line "" [ [(s,blackScholesCall s 100 0.001 r v) | s <- ss] ]
+    plot $ line lbl [ [(s,blackScholesCall s 100 t r v) | s <- ss] ]
+  where    
+    ss = [50,51..150]
+    lbl = "t = " ++ show t ++ ", r = " ++ show r
 
-main = renderableToFile def "example2_big.png" chart
+-- Construct a grid of charts, with a single title accross the top
+grid = title `wideAbove` aboveN [ besideN [ layoutToGrid (bsChart t r v) | t <-ts ] | r <- rs ]
+  where
+    ts = [1,2,5]
+    rs = [0.05,0.10,0.20]
+    v = 0.10
+    title = setPickFn nullPickFn $ label ls HTA_Centre VTA_Centre "Optimizacion de la malla a mayor carga academica"
+    ls = def { _font_size   = 15 , _font_weight = FontWeightBold }
+    
+main :: IO ()
+main = do
+  void $ renderableToFile def "example13_big.png" $ fillBackground def $ gridToRenderable $  grid
