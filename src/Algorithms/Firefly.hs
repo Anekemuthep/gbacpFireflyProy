c23d5bfcc54e2b5987479f2421c8a4854253933c
Alfonso Bustamante <athal.zigma@gmail.com>
2018-03-12 17:02:04 -0300

    change in intensity function

--- src/Algorithms/Firefly.hs
+++ src/Algorithms/Firefly.hs
@@ -14,7 +14,7 @@ restNum xs = xs ++ xs
 intensity :: Float -> Float -> Float
 intensity x1 x2 =
     let dist = x1 - x2
-    in 1*(exp(-1*(dist)^2))*dist
+    in 1*(exp(-1*dist))*dist
 
 mov :: Float -> Float -> Float -> Float -> Float
 mov x1 x2 alpha aleat = intensity x1 x2 + x1 + alpha*(aleat-0.5)
