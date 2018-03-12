4fe20469b77087f21eede3be03e71eca8375265d
Alfonso Bustamante <athal.zigma@gmail.com>
2018-03-12 18:47:40 -0300

    minor changes (modification on number of variables of mov general type)

--- src/Algorithms/Firefly.hs
+++ src/Algorithms/Firefly.hs
@@ -16,7 +16,7 @@ intensity x1 x2 =
     let dist = x1 - x2
     in  exp(-1*dist)
 
-mov :: Float -> Float -> Float -> Float -> Float
+mov :: Float -> Float -> Float -> Float
 mov x1 x2 aleat = (intensity x1 x2)*(x1-x2) + x1 + 1*(aleat-0.5)
 
 -- | The sigmoid function:  1 / (1 + exp (-x))
