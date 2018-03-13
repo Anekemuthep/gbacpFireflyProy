42c1823fe90ee8c66a086fd1421175474f497a0e
Alfonso Bustamante <athal.zigma@gmail.com>
2018-03-13 06:46:15 -0300

    minor changes

--- src/Diagramas/DiagramIntensity.hs
+++ src/Diagramas/DiagramIntensity.hs
@@ -14,5 +14,5 @@ signal2 xs = [ (x, exp(-1*x)) | x <- xs]
 main = toFile def "/Users/alfonsobustamante/mychart.svg" $ do
     layout_title .= "Intensity function with different values of gamma"
     setColors [opaque blue, opaque red]
-    plot (line "intensity with FFA definition" [signal1 [0,(-1)..1]])
-    plot (points "intensity with -alpha" (signal2 [0,(-1)..1]))
+    plot (line "intensity with FFA definition" [signal1 [(-1)..1]])
+    plot (points "intensity with -alpha" (signal2 [(-1)..1]))
