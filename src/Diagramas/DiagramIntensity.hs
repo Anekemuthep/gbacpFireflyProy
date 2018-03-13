e171bacbd9370b06e047a05fbe9861c728d6cf9b
Alfonso Bustamante <athal.zigma@gmail.com>
2018-03-13 07:01:04 -0300

    se agrego un nuevo grafico a DiagramIntensity

--- src/Diagramas/DiagramIntensity.hs
+++ src/Diagramas/DiagramIntensity.hs
@@ -11,8 +11,12 @@ signal1 xs = [ (x, exp(-1*(abs x))) | x <- xs ]
 signal2 :: [Double] -> [(Double, Double)]
 signal2 xs = [ (x, exp(-1*x)) | x <- xs]
 
+signal3 :: [Double] -> [(Double, Double)]
+signal3 xs = [ (x, exp(abs x)) | x <- xs]
+
 main = toFile def "/Users/alfonsobustamante/mychart.svg" $ do
     layout_title .= "Intensity function with different values of gamma"
-    setColors [opaque blue, opaque red]
-    plot (line "intensity with FFA definition" [signal1 [(-1),(-0.9)..1]])
-    plot (points "intensity with -alpha" (signal2 [(-1),(-0.9)..1]))
+    setColors [opaque blue, opaque red, opaque green]
+    plot (line "intensity with FFA definition" [signal1 [(-1),(-0.09)..1]])
+    plot (points "intensity with -alpha" (signal2 [(-1),(-0.09)..1]))
+    prot (line "intensity with Binary FFA definition" (signal3 [(-1),(-0.09)..1]))
