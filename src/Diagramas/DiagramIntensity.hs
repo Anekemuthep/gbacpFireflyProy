97124d94eabd4a836c76449e2a7e7f396fe2e05d
Alfonso Bustamante <athal.zigma@gmail.com>
2018-03-13 06:34:15 -0300

    Se agregaron funciones a DiagramIntensity

--- src/Diagramas/DiagramIntensity.hs
+++ src/Diagramas/DiagramIntensity.hs
@@ -5,11 +5,14 @@ import Lib.Prelude
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Diagrams(toFile)
 
-signal :: [Double] -> [(Double,Double)]
-signal xs = [ (x,(sin (x*3.14159/45) + 1) / 2 * (sin (x*3.14159/5))) | x <- xs ]
+signal1 :: [Double] -> [(Double,Double)]
+signal1 xs = [ (x, exp(-1*(abs x))) | x <- xs ]
+
+signal2 :: [Double] -> [(Double, Double)]
+signal2 xs = [ (x, exp(-1*x)) | x <- xs]
 
 main = toFile def "/Users/alfonsobustamante/mychart.svg" $ do
-    layout_title .= "Amplitude Modulation"
+    layout_title .= "Intensity function with different values of gamma"
     setColors [opaque blue, opaque red]
-    plot (line "am" [signal [0,(0.5)..400]])
-    plot (points "am points" (signal [0,7..400]))
+    plot (line "intensity with FFA definition" [signal1 [0,(-1)..1]])
+    plot (points "intensity with -alpha" (signal2 [0,(-1)..1]))
