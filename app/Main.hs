d28a34b8a36855ae1a07963ccae110d58d776b76
Alfonso Bustamante <athal.zigma@gmail.com>
2018-06-15 17:46:07 -0300

    Test VIII

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Protolude
+--import Protolude
 --import Lib
     
-import Web.Spock
-import Web.Spock.Config
-
-import  qualified Data.Semigroup as SG
-import Control.Monad.Trans
-import Data.Monoid
-import Data.IORef
-import qualified Data.Text as T
-
-data MySession = EmptySession
-data MyAppState = DummyAppState (IORef Int)
+import System.Environment
+import Web.Spock.Safe
 
 main :: IO ()
-main =
-    do ref <- newIORef 0
-       spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
-       runSpock 8080 (spock spockCfg app)
-
-app :: SpockM () MySession MyAppState ()
-app =
-    do get root $
-           text "Hello World!"
-       get ("hello" <//> var) $ \name ->
-           do (DummyAppState ref) <- getState
-              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-              text ("Hello " SG.<> name SG.<> ", you are visitor number " SG.<> T.pack (show visitorNumber))
+main = do
+    env <- getEnvironment
+    let port = maybe 8080 read $ lookup "PORT" env
+    runSpock port $ spockT id $
+      get "/" $ text "Hello, world!"
 
 -- module Main where
 
